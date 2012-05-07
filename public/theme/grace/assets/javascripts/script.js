6182c55aff36c9769f6bc53b35051aec21c1e319
Seiei Higa <hanachin@gmail.com>
2012-05-08 08:50:48 +0900

    remove grace theme and lokka-amazon_associate

--- public/theme/grace/assets/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".social-icon img").hover(function() {
-        $(this).parent().next("span").animate({opacity: "show", top: "170"}, "slow");}, function(){
-            $(this).parent().next("span").animate({opacity: "hide", top: "170"}, "fast");
-    });
-
-    $().UItoTop({ easingType: 'easeOutQuart' });
-});
