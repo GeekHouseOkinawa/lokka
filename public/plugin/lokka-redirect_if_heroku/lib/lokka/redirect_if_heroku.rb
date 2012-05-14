5213baaffa99c9ee0386cd29c51f50e47ed815a2
Seiei Higa <hanachin@gmail.com>
2012-05-14 14:01:42 +0900

    redirect if heroku

---
+++ public/plugin/lokka-redirect_if_heroku/lib/lokka/redirect_if_heroku.rb
@@ -0,0 +1,12 @@
+# Redirect if Heroku plugin.
+# http://blog.champierre.com/911
+module Lokka
+  module RedirectIfHeroku
+    def self.registered(app)
+      app.before do
+        site_url = ENV['SITE_URL'] || 'http://heroku.com'
+        redirect site_url, 301 if request.host =~ /\.heroku\.com$/
+      end
+    end
+  end
+end
